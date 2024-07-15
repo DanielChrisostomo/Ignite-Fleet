bd2c1796b52b2adbccfd17eba1b7189af41c755a
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-15 17:13:03 -0300

    Módulo Cloud Mongodb Atlas Completo

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,23 +1,36 @@
 import { TouchableOpacity } from "react-native";
 import { Power } from "phosphor-react-native"
+import { useUser, useApp } from "@realm/react";
 
-import { Container, Greeting, Message, Name } from './styles';
 import theme from "../../theme";
 
+import { Container, Greeting, Message, Name, Picture } from './styles';
+
+const blurhash = "L17_1hkCW=of00ayjZay~qj[ayjt"
+// blurhash Site = https://blurha.sh/
+
 export function HomeHeader() {
+  const user = useUser()
+  const app = useApp()
+
+  function handleLogOut () {
+    app.currentUser?.logOut()
+  }
+
   return (
     <Container>
+      <Picture source={{uri: user?.profile.pictureUrl }} placeholder={{blurhash}} />
       <Greeting>
         <Message>
           Olá
         </Message>
 
         <Name>
-          Rodrigo
+          {user?.profile.name}
         </Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
         <Power size={32} color={theme.COLORS.GRAY_400}/>
       </TouchableOpacity>
     </Container>
