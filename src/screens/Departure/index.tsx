9664a978829a192448b5f9a4dab1ae75fb12141a
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-22 12:06:32 -0300

    Módulo Geolocalização completo

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { TextInput, ScrollView, KeyboardAvoidingView, Platform, Alert } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/native";
+import { CarSimple } from 'phosphor-react-native';
+import { useForegroundPermissions, watchPositionAsync, LocationAccuracy, LocationSubscription } from "expo-location"
 
 import { useUser } from "@realm/react";
 import { useRealm } from "../../libs/realm";
 import { Historic } from "../../libs/realm/schemas/Historic";
 
 import { licensePlateValidate } from "../../utils/licensePlateValidate";
+import { getAddressLocation } from "../../utils/getAddressLocation";
 
+
+import { Loading } from "../../components/Loading";
+import { LocationInfo } from "../../components/LocationInfo";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 
-import { Container, Content } from "./styles";
-import { useNavigation } from "@react-navigation/native";
+import { Container, Content, Message } from "./styles";
 
 export function Departure() {
   const [ description, setDescription ] = React.useState("")
   const [ licensePlate, setLicensePlate ] = React.useState("")
   const [ isRegistering, setisRegistering ] = React.useState(false)
+  const [isLoadingLocation, setIsLoadingLocation] = React.useState(true)
+  const [currentAddress, setCurrentAddress] = React.useState<string | null>(null)
+
+  const [locationForegroundPermission, requestLocationForegroundPermission] = useForegroundPermissions()
 
   const { goBack } = useNavigation()
   const realm = useRealm()
@@ -61,6 +71,52 @@ export function Departure() {
     setisRegistering(false);
   }
 
+  React.useEffect(() => {
+    requestLocationForegroundPermission()
+  }, [])
+
+  React.useEffect(() => {
+    if(!locationForegroundPermission?.granted){
+      return
+    } 
+
+    let subscription: LocationSubscription;
+
+    watchPositionAsync({
+      accuracy: LocationAccuracy.High,
+      timeInterval: 1000
+    }, (location) => {
+       getAddressLocation(location.coords)
+        .then(address => {
+          if(address) {
+            setCurrentAddress(address)
+          }
+        }).finally(() => setIsLoadingLocation(false))
+    }).then(response => subscription = response);
+
+    return () => {
+      if(subscription)
+      subscription.remove()
+    };
+  }, [locationForegroundPermission?.granted])
+
+  if(!locationForegroundPermission?.granted) {
+    return (
+      <Container>
+        <Header title="Saída" />
+        <Message>
+        Você precisa permitir que o aplicativo tenha acesso a 
+          localização para acessar essa funcionalidade. Por favor, acesse as
+          configurações do seu dispositivo para conceder a permissão ao aplicativo.
+        </Message>
+      </Container>
+    )
+  }
+
+  if(isLoadingLocation) {
+    return <Loading />
+  }
+
   return (
     <Container>
       <Header title="Saída" />
@@ -68,6 +124,14 @@ export function Departure() {
       <KeyboardAwareScrollView extraHeight={100}>
         <ScrollView>
           <Content>
+          {
+              currentAddress &&
+              <LocationInfo
+                icon={CarSimple}
+                label='Localização atual'
+                description={currentAddress}
+              />
+            }
             <LicensePlateInput
               ref={licensePlateRef}
               label="Placa do veículo"
